1a83c26bf01cf24682b63cb08c57f7c694dacb54
DmitriyBadeev <dmitriibadeev@gmail.com>
2021-12-05 22:01:10 +0500

    Включение Homepage

--- Sigma.Identity.Web/Quickstart/Home/HomeController.cs
+++ Sigma.Identity.Web/Quickstart/Home/HomeController.cs
@@ -29,14 +29,16 @@ namespace IdentityServerHost.Quickstart.UI
 
         public IActionResult Index()
         {
-            if (_environment.IsDevelopment())
-            {
-                // only show in development
-                return View();
-            }
+            return View();
+            
+            // if (_environment.IsDevelopment())
+            // {
+            //     // only show in development
+            //     return View();
+            // }
 
-            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
-            return NotFound();
+            // _logger.LogInformation("Homepage is disabled in production. Returning 404.");
+            // return NotFound();
         }
 
         /// <summary>
